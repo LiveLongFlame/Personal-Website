4afdb10dd32123c41c029963182bd25ac5256c6b
Valentino Osorio Schwarz <111870582+LiveLongFlame@users.noreply.github.com>
2024-12-19 12:20:22 +0000

    Change styleing of about me div and added more information to the about me text

--- IndexPage_js.js
+++ IndexPage_js.js
@@ -1,11 +1,6 @@
 let aboutMeVisible = false;
 let myCodeVisible = false;
 
-function about_me_btn_click() {
-    const aboutMeText = document.getElementById("about-me-text");
-    aboutMeVisible = !aboutMeVisible;
-    aboutMeText.style.display = aboutMeVisible ? "block" : "none";
-}
 
 function my_code_btn() {
     const myCodeText = document.getElementById("my-code-text");
